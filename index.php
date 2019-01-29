676cb1015775f0da9157a8778927d08f045d9dff
andrea-delas <andrea.delas@outlook.com>
2019-01-29 10:48:43 +0100

    zaposlenik.php

--- index.php
+++ index.php
@@ -1,142 +1,32 @@
+
 <?php
+require "zaposlenik.php";
 
-class Zaposlenik
+$popisZaposlenika = [];
+
+while (true)
 {
-    protected $id;
-    protected $ime;
-    protected $prezime;
-    protected $datumRodenja;
-    protected $spol;
-    protected $iznosMjesecnihPrimanja;
-    protected static $popisSvihZaposlenika = array();
 
-    function __construct($id, $ime, $prezime, $datumRodenja, $spol, $iznosMjesecnihPrimanja)
-    {
-        $this->id = $id;
-        $this->ime = $ime;
-        $this->prezime = $prezime;
-        $this->datumRodenja = $datumRodenja;
-        $this->spol = $spol;
-        $this->iznosMjesecnihPrimanja = $iznosMjesecnihPrimanja;
-        return Zaposlenik::$popisSvihZaposlenika[]= $this;
+prikaziIzbornik();
 
-    }
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getIme()
-    {
-        return $this->ime;
-    }
-
-    /**
-     * @param mixed $ime
-     */
-    public function setIme($ime)
-    {
-        $this->ime = $ime;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPrezime()
-    {
-        return $this->prezime;
-    }
-
-    /**
-     * @param mixed $prezime
-     */
-    public function setPrezime($prezime)
-    {
-        $this->prezime = $prezime;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDatumRodenja()
-    {
-        return $this->datumRodenja;
-    }
-
-    /**
-     * @param mixed $datumRodenja
-     */
-    public function setDatumRodenja($datumRodenja)
-    {
-        $this->datumRodenja = $datumRodenja;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSpol()
-    {
-        return $this->spol;
-    }
-
-    /**
-     * @param mixed $spol
-     */
-    public function setSpol($spol)
-    {
-        $this->spol = $spol;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getIznosMjesecnihPrimanja()
-    {
-        return $this->iznosMjesecnihPrimanja;
-    }
-
-    /**
-     * @param mixed $iznosMjesecnihPrimanja
-     */
-    public function setIznosMjesecnihPrimanja($iznosMjesecnihPrimanja)
-    {
-        $this->iznosMjesecnihPrimanja = $iznosMjesecnihPrimanja;
-    }
-
-}
+$choice = trim(fgets(STDIN));
 
 
-while( true ) {
-
-    prikaziIzbornik();
-
-    $choice = trim(fgets(STDIN));
-
-
-    switch ($choice) {
+switch ($choice)
+{
 
         case 1:
             {
-
+                pregledZaposlenika($popisZaposlenika);
 
                 break;
             }
+
         case 2:
             {
+                echo "Upišite sve potrebne podatke: \n";
+                $popisZaposlenika[] = unosNovogZaposlenika($popisZaposlenika);
+
                 break;
             }
         case 3:
@@ -148,7 +38,8 @@ while( true ) {
                 break;
             }
         case 5:
-            {  prikaziPodIzbornik();
+            {
+                prikaziPodIzbornik();
                 $choice = trim(fgets(STDIN));
 
             }
@@ -158,53 +49,59 @@ while( true ) {
             }
     }
 }
-function prikaziIzbornik() {
+function prikaziIzbornik()
+{
 
-echo "************ Registar zaposlenika ******************\n";
-echo "1 - Pregled zaposlenika\n";
-echo "2 - Unos novog zaposlenika\n";
-echo "3 - Promjena podataka postojećem zaposleniku\n";
-echo "4 - Brisanje zaposlenika\n";
-echo "5 - Statistika\n";
-echo "************ Registar zaposlenika ******************\n";
-echo "Odaberite brojeve od 1 do 5:";
+    echo "************ Registar zaposlenika ******************\n";
+    echo "1 - Pregled zaposlenika\n";
+    echo "2 - Unos novog zaposlenika\n";
+    echo "3 - Promjena podataka postojećem zaposleniku\n";
+    echo "4 - Brisanje zaposlenika\n";
+    echo "5 - Statistika\n";
+    echo "************ Registar zaposlenika ******************\n";
+    echo "Odaberite brojeve od 1 do 5:";
+}
+function unosNovogZaposlenika ()
+{
+
+    echo "Unesite ID zaposlenika: ";
+    $id = trim(fgets(STDIN));
+
+    echo "Unesite ime zaposlenika: ";
+    $ime = trim(fgets(STDIN));
+
+    echo "Unesite prezime zaposlenika: ";
+    $prezime = trim(fgets(STDIN));
+
+    echo "Unesite datum rođenja zaposlenika: ";
+    $datumRodenja = trim(fgets(STDIN));
+
+    echo "Unesite spol zaposlenika: ";
+    $spol = trim(fgets(STDIN));
+
+    echo "Unesite iznos mjesečnih primanja zaposlenika: ";
+    $iznosMjesecnihPrimanja = trim(fgets(STDIN));
+
+    $p[] = new Zaposlenik($id, $ime, $prezime, $datumRodenja, $spol, $iznosMjesecnihPrimanja);
+
+
 }
 
-while( true ) {
+function pregledZaposlenika($array)
 
-
-
-
-    switch ($choice) {
-
-        case 1:
-            {
-
-
-                break;
-            }
-        case 2:
-            {
-                break;
-            }
-        case 3:
-            {
-                break;
-            }
-        case 4:
-            {
-                break;
-            }
-        case 5:
-            {
-                break;
-            }
-        default:
-            {
-
-            }
+{
+    echo "*************************************************\n";
+    for ($i = 0; $i < count($array); $i++) {
+        echo "ID: " . $array[$i]->getId() . "\n";
+        echo "IME: " . $array[$i]->getIme() . "\n";
+        echo "PREZIME: " . $array[$i]->getPrezime() . "\n";
+        echo "DATUM ROĐENJA: " . $array[$i]->getDatumRodenja() . "\n";
+        echo "SPOL: " . $array[$i]->getSpol() . "\n";
+        echo "MJESEČNA PRIMANJA: " . $array[$i]->getiznosMjesecnihPrimanja() . "\n";
+        echo "*************************************************\n";
     }
 }
+
 function prikaziPodIzbornik()
 {
 
--- zaposlenik.php
+++ zaposlenik.php
@@ -1,7 +1,79 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andrea
- * Date: 27.01.19.
- * Time: 18:27
- */
+
+class Zaposlenik
+{
+    protected $id;
+    protected $ime;
+    protected $prezime;
+    protected $datumRodenja;
+    protected $spol;
+    protected $iznosMjesecnihPrimanja;
+
+    public function __construct($id, $ime, $prezime, $datumRodenja, $spol, $iznosMjesecnihPrimanja)
+    {
+        $this->id = $id;
+        $this->ime = $ime;
+        $this->prezime = $prezime;
+        $this->datumRodenja = $datumRodenja;
+        $this->spol = $spol;
+        $this->iznosMjesecnihPrimanja = $iznosMjesecnihPrimanja;
+
+
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        if (preg_match('/^[1-9][0-9]*$/', $id)) {
+
+            echo "Broj je ispravan";
+        } else {
+
+            echo " Molimo unesite ispravan broj";
+        }
+    }
+    public function getIme()
+    {
+        return $this->ime;
+    }
+    public function setIme($ime)
+    {
+        $this->ime = $ime;
+    }
+    public function getPrezime()
+    {
+        return $this->prezime;
+    }
+    public function setPrezime($prezime)
+    {
+        $this->prezime = $prezime;
+    }
+    public function getDatumRodenja()
+    {
+        return $this->datumRodenja;
+    }
+    public function setDatumRodenja($datumRodenja)
+    {
+        $this->datumRodenja = $datumRodenja;
+    }
+    public function getSpol()
+    {
+        return $this->spol;
+    }
+    public function setSpol($spol)
+    {
+        $this->spol = $spol;
+    }
+    public function getIznosMjesecnihPrimanja()
+    {
+        return $this->iznosMjesecnihPrimanja;
+    }
+    public function setIznosMjesecnihPrimanja($iznosMjesecnihPrimanja)
+    {
+        $this->iznosMjesecnihPrimanja = $iznosMjesecnihPrimanja;
+    }
+}
